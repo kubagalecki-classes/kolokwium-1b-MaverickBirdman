e0d7b475302b9f0ae3c848a7e4fe2475270e452c
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2021-01-15 08:45:10 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,17 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+
+template <typename T>
+class ArtystaPlusInstrument
+{
+public:
+	ArtystaPlusInstrument(const Artysta& aa, const T& bb) :a(aa),i(bb) {}
+	unsigned koncert()
+	{
+		i.liczbaUczestnikow(a);
+	}
+private:
+	Artysta a;
+	T i;
+};
