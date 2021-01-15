8ed5b9da6f3de99de4d5b0afdd0034602b0d18b6
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2021-01-15 08:56:08 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,14 +7,16 @@
 #include <string>
 using namespace std;
 // tutaj klasa Artysta
+using namespace std;
+
 class Artysta	//podaj nazwe klasy
 {
 public:
 	Artysta():pseudonim("Anonim"), mls(0) {}
-	Artysta(const string& a, unsigned b): pseudonim(a), mls(b) {}
+	Artysta(const string& a, unsigned b) :pseudonim(a), mls(b) {}
 	void graj(std::ostream& stream) const 
 	{
-		std::stream << pseudonim<<": "<<mls;
+		stream << pseudonim<<": "<<mls;
 	}
 	const string& getPseudonim(std::ostream& o) const { return pseudonim; }
 	unsigned getSluchacze() const { return mls; }
