3cb1374dd619802ef0e74b946d0f648b8f4438c9
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2021-01-15 09:12:29 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,14 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+vector<Artysta> stworzZespol(int a, const std::string& s)
+{
+	vector<Artysta> v(a);
+	if (s.size() != a)
+	{
+		std::logic_error;
+	}
+	v[0] = Artysta(s, 1);
+	v[1] = Artysta(s, 2);
+	v[2] = Artysta(s, 3);
+}
