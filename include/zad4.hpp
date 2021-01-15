c9e0a0ebaf163788b43a8ee789fcccd4005fbd9b
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2021-01-15 09:19:16 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -9,7 +9,7 @@ vector<Artysta> stworzZespol(int a, const std::string& s)
 	vector<Artysta> v(a);
 	if (s.size() != a)
 	{
-		std::logic_error;
+		std::logic_error("abc");
 	}
 	v[0] = Artysta(s, 1);
 	v[1] = Artysta(s, 2);
